20054adcedca982fb39ba1030aeb8200df37c0aa
hjseo <mijin2197@hanmail.net>
2021-06-10 23:31:37 +0900

    About me 앱 제작 (사진, 정보, 스킬 출력)

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -9,25 +9,138 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'First Flutter',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange[300],
+      appBar: AppBar(
+        title: Text('About Me'),
+        backgroundColor: Colors.deepOrange[200],
+        elevation: 0.0, //입체감 제거
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/hj1.PNG'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.8,
+              endIndent: 30.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'HUIJU SEO',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'BIRTHDAY(AGE)',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '1993.05.21(29)',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text('Java',
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text('JavaScript',
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text('RDBMS',
+                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/hj2.PNG'),
+                radius: 40.0,
+                backgroundColor: Colors.deepOrange[300],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 //Scaffold가 없으면 앱 화면 배치를 전혀 할 수 없다.
 //하나의 요소라도 동적이면 StatefulWidget을 사용해야한다.
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter First App')),
-      body: Center(
-        child: Column(
-          children: [Text('Hello'), Text('Hello'), Text('Hello')],
-        ),
-      ),
-    );
-  }
-}
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Flutter First App')),
+//       body: Center(
+//         child: Column(
+//           children: [Text('Hello'), Text('Hello'), Text('Hello')],
+//         ),
+//       ),
+//     );
+//   }
+// }
